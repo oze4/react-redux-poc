7d8295dfede1f8146fe419695b6b9884c6eea982
oze4 <matthewpoestreich@gmail.com>
2019-08-02 13:26:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import { withBackgroundColorState } from './redux/containers';
 function App(props) {
   return (
     <div style={{ margin: '100px', backgroundColor: props.backgroundColor }}>
+      <h5>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/oze4/react-redux-poc">
+          GitHub Repo
+        </a>
+      </h5>
       <AppCounter title="Counter" />
       <AppCounterDisplay />
     </div>
